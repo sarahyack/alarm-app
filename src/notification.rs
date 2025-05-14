2a3e20ea19735ae068141b6af9e3d3141aacf74a
sarahyack <sarah@yack.email>
2025-05-14 13:35:22 -0600

    Added Action to Test Toast

--- src/notification.rs
+++ src/notification.rs
@@ -1,19 +1,8 @@
 // use crate::alarm::Alarm;
 
 use winrt_toast_reborn::content::text::TextPlacement;
+use winrt_toast_reborn::content::action::Action;
 use winrt_toast_reborn::{Header, Text, Toast, ToastManager};
-
-// pub fn send_notification(alarm: &Alarm) {
-//     let toast = Toast::new("Alarm App")
-//         .title("Alarm Triggered!")
-//         .text1(format!("Alarm time: {}", alarm.time.format("%I:%M %p")))
-//         .duration(Duration::Long)
-//         .action(Action::new("Dismiss", "Dismiss"))
-//         .action(Action::new("Snooze", "Snooze"))
-//         .sound(Some(winrt_notification::Sound::Default))
-//         .show()
-//         .expect("Failed to show toast");
-// }
 
 pub fn send_toast() {
     let manager = ToastManager::new("SarahY.AlarmApp");
@@ -22,7 +11,8 @@ pub fn send_toast() {
     toast
         .text1("Hello, world!")
         .text2(Text::new("Body"))
-        .text3(Text::new("Via SMS").with_placement(TextPlacement::Attribution));
+        .text3(Text::new("Via SMS").with_placement(TextPlacement::Attribution))
+        .action(Action::new("Dismiss", "dismiss", ""));
 
     manager.show(&toast).expect("Failed to show toast");
 }
