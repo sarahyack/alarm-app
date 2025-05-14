81f06380edb9b4280712f6793a4636ca705ab926
sarahyack <sarah@yack.email>
2025-05-14 13:34:57 -0600

    Updated Icon Path to Global Utils Icon Path

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,12 @@
 // mod alarm;
 mod notification;
 // mod scheduler;
+mod utils;
 
-use std::path::Path;
 use winrt_toast_reborn::register;
 
 pub fn main() {
-    let path = Path::new(env!("CARGO_MANIFEST_DIR"))
-        .join("assets/images")
-        .join("alarm-app-icon.ico");
-    register("SarahY.AlarmApp", "Alarm App", Some(&path)).expect("Failed to register app");
+    register("SarahY.AlarmApp", "Alarm App", Some(&utils::APP_ICON)).expect("Failed to register app");
 
     notification::send_toast();
 }
